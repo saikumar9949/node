0fea8c225f02ccfd9442923d8ce45e37d55f9b84
saikumar9949 <108319978+saikumar9949@users.noreply.github.com>
2022-11-17 19:44:18 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,58 +1,48 @@
 const http = require("http");
 const fs = require("fs");
 
-var port = require("minimist")(process.argv.slice(2),{
-    default:{
-        greeting:"Hello",
-    },
-});
-
 let homeContent = "";
 let projectContent = "";
-let registrarionContent = "";
-// fs.readFile("home.html",(err, home) => {
-    // console.log(home.toString());
-// });
-fs.readFile("home.html", (err, home)=>{
-    if(err){
-        throw err;
-    }
-    homeContent=home;
-    // http
-    // .createServer((request,response)=>{
-    //     response.writeHeader(200, {"Content-Type":"text/html" });
-    //     response.write(home);
-    //     response.end();
-    // })
-    // .listen(3000);
+let reg="";
+fs.readFile("home.html", (err, home) => {
+  if (err) {
+    throw err;
+  }
+  homeContent = home;
 });
-fs.readFile("project.html", (err,project)=>{
+
+fs.readFile("project.html", (err, project) => {
+  if (err) {
+    throw err;
+  }
+  projectContent = project;
+});
+
+fs.readFile("registration.html",
+(err,regf) => {
     if(err) {
         throw err;
     }
-    projectContent = project;
+    reg=regf;
 });
-fs.readFile("registration.html", (err,registration)=>{
-    if(err) {
-        throw err;
+let args=require("minimist")(process.argv.slice(2));
+http
+  .createServer((request, response) => {
+    let url = request.url;
+    response.writeHeader(200, { "Content-Type": "text/html" });
+    switch (url) {
+      case "/project":
+        response.write(projectContent);
+        response.end();
+        break;
+      case "/registration":
+        response.write(reg);
+        response.end();
+        break;
+      default:
+        response.write(homeContent);
+        response.end();
+        break;
     }
-    registrarionContent=registration;
-})
-http.createServer((request,response)=>{
-        let url = request.url;
-        response.writeHeader(200, { "Content-Type":"text/html" });
-        switch(url){
-            case "/registration":
-              response.write(registrarionContent);
-              response.end();
-              break;
-            case "/project":
-              response.write(projectContent);
-             response.end();
-              break;
-            default:
-            response.write(homeContent);
-            response.end();
-            break;
-        }
-    }).listen(port);
+  })
+  .listen(args["port"]);
